eea33395d7a052dec8fa9e3ec28cb17d6c1fc165
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-09 15:17:57 -0300

    Aula Adapters

--- Exemplo/Exemplo/Program.cs
+++ Exemplo/Exemplo/Program.cs
@@ -16,11 +16,18 @@ namespace Exemplo
             var con = new SqlConnection(constr);
             var SQL = "select * from CLIENTES";
             var cmd = new SqlCommand(SQL, con);
-            var ds = new DataSet();
-            var da = new SqlDataAdapter();
-
-
-
+            var ds = new DataSet("CADASTRO");
+            var da = new SqlDataAdapter(cmd);
+            da.Fill(ds,"CLIENTES");
+            var dt = ds.Tables[0];
+            foreach(DataRow row in dt.Rows)
+            {
+                Console.WriteLine("IDCliente: " + row[0].ToString());
+                Console.WriteLine("Nome: " + row[1].ToString());
+                Console.WriteLine("Email: " + row[2].ToString());
+            }
+            dt.WriteXml("dados.xml");
+            Console.ReadKey();
         }
     }
 }
