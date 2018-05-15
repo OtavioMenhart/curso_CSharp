b8bcefe5604c3ae42b4fc0ab1f75e2cd1673ffa9
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-15 16:18:39 -0300

    Classes

---
+++ ExemploClasses/ExemploClasses/Calculadora.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Menhart.Util
+{
+    class Calculadora
+    {
+        public int Somar(int x, int y)
+        {
+            return x + y; //business
+        }
+    }
+}
---
+++ ExemploClasses/ExemploClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Menhart.Util;
+
+namespace ExemploClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xStr = Console.ReadLine(); //UI
+            string yStr = Console.ReadLine(); //UI
+
+            int x = Convert.ToInt32(xStr);
+            int y = Convert.ToInt32(yStr);
+
+            Calculadora calc = new Calculadora();
+
+            int r = calc.Somar(x, y);
+            Console.WriteLine(r.ToString()); //UI
+            Console.ReadKey();
+        }
+
+    }
+}
