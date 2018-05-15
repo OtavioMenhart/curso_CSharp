19b0beb5e6ae373da88e3dcec4b3d0cdaabecef5
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-15 12:45:33 -0300

    Serviço e Cliente - Web API

---
+++ ServicoWebAPI/ServicoWebAPI/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ServicoWebAPI.Models;
+
+namespace ServicoWebAPI.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        Cliente[] clientes = new Cliente[]
+        {
+            new Cliente(){ ID = 1, Nome = "Otávio", EMail = "otaviomenhart@hotmail.com" },
+            new Cliente(){ ID = 2, Nome = "Teste", EMail = "teste@gmail.com"},
+            new Cliente(){ ID = 3, Nome = "Teste3", EMail = "teste3@gmail.com"}
+        };
+
+        public IEnumerable<Cliente> getClientes()
+        {
+            return clientes;
+        }
+
+        //rota
+        public IHttpActionResult getCliente(int id)
+        {
+            var cli = clientes.FirstOrDefault((c) => c.ID == id);
+            return Ok(cli);
+        }
+    }
+}
---
+++ ServicoWebAPI/ServicoWebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServicoWebAPI.WebApiApplication" Language="C#" %>
