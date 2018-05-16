58451fc185a5c2ad328e5a049af46a0609e01716
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-16 13:37:58 -0300

    Encapsulamento em C#

---
+++ Encapsulamento/Encapsulamento/Conta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDoBrasil.Contas
+{
+    public enum TipoConta { ContaCorrente, ContaPoupanca};
+    public class Conta
+    {
+        //atributos
+        public string Num;
+        private double _saldo; //encapsulamento
+        public TipoConta Tipo;
+        
+        //construtor
+        public Conta(TipoConta tipo, string num)
+        {
+            this.Num = num;
+            this.Tipo = tipo;
+            this._saldo = 0;
+        }
+
+        //métodos
+        public void Sacar(double valor)
+        {
+            _saldo = _saldo - valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo = _saldo + valor;
+        }
+
+        public double getSaldo()
+        {
+            return _saldo;
+        }
+    }
+}
---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BancoDoBrasil.Contas;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta(TipoConta.ContaCorrente, "123456");
+
+            #region Modo public
+            //conta.Num = "123";
+            //conta.Saldo = 0;
+            //conta.Tipo = TipoConta.ContaCorrente;
+            #endregion
+
+            conta.Depositar(500);
+            conta.Sacar(300);
+
+            Console.WriteLine("Saldo: " + conta.getSaldo().ToString());
+            Console.ReadKey();
+        }
+    }
+}
