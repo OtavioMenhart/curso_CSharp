fdef143e6c72facbcc500b3070031ef4ab6ad4c1
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-16 12:19:43 -0300

    Herança em C#

---
+++ Heranca/Heranca/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocha.Entidades
+{
+    public class Cliente
+    {
+        //atributos
+        public int Codigo;
+        public string Nome;
+       // public int TipoCliente; type code não é uma boa prática
+
+        //métodos
+        public void Mostrar()
+        {
+            Console.WriteLine("Dados do cliente: {0}--{1}", Codigo, Nome);
+        }
+    }
+
+    public class PessoaFisica : Cliente
+    {
+        public string CPF;
+        public void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("CPF: {0}", CPF);
+        }
+    }
+
+    public class PessoaJuridica : Cliente
+    {
+        public string CNPJ;
+        public void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("CNPJ: {0}", CNPJ);
+        }
+    }
+
+}
---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocha.Entidades;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaFisica cli1 = new PessoaFisica();
+            cli1.Codigo = 123;
+            cli1.Nome = "Otávio";
+            cli1.CPF = "123456789";
+            cli1.Mostrar();
+
+            PessoaJuridica cli2 = new PessoaJuridica();
+            cli2.Codigo = 1234;
+            cli2.Nome = "juridica";
+            cli2.CNPJ = "123456789456789";
+            cli2.Mostrar();
+
+            Console.ReadKey();
+        }
+    }
+}
