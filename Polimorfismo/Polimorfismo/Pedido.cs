b2bf95ac86a30d356c2aa24069575e3d14398550
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-17 12:12:19 -0300

    Polimorfismo e Override (método de pagamento)

---
+++ Polimorfismo/Polimorfismo/Pedido.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public abstract class FormaPagamento
+    {
+        public abstract void Pagar(double valor);
+    } 
+
+
+    public class Boleto : FormaPagamento
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago valor de {0} no boleto", valor);
+        }
+    }
+
+    public class Cartao : FormaPagamento
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago valor de {0} no cartão", valor);
+        }
+    }
+
+    public class Pedido
+    {
+        private FormaPagamento _forma;
+
+        public void Pagar(double valor)
+        {
+            //delegação polimórfica
+            this._forma.Pagar(valor);
+        }
+
+        public Pedido(FormaPagamento formaPagamento)
+        {
+            this._forma = formaPagamento;
+        }
+    }    
+}
---
+++ Polimorfismo/Polimorfismo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pedido pedido = new Pedido(new Cartao()); //injeto a dependência
+            double valor = 2000;
+            pedido.Pagar(valor);
+            Console.ReadLine();
+        }
+    }
+}
