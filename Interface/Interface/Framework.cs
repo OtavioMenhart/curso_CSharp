de0826acaa6558201cf80d494ae390f2648d1226
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-21 12:10:20 -0300

    Interface em C#

---
+++ Interface/Interface/Framework.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface INotificacao
+    {
+        void Enviar(string Mensagem);
+    }
+
+    public abstract class Notificacao : INotificacao
+    {
+        public abstract void Enviar(string Mensagem);
+    }
+
+    public class EMail : Notificacao
+    {
+        public override void Enviar (string Mensagem)
+        {
+            Console.WriteLine("Enviando email: " + Mensagem);
+        }
+    }
+
+    public class SMS : Notificacao
+    {
+        public override void Enviar (string Mensagem)
+        {
+            Console.WriteLine("Enviando SMS: " + Mensagem);
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome;
+    }
+
+    public class Funcionario : Pessoa, INotificacao
+    {
+        public void Enviar(string Mensagem)
+        {
+            Console.WriteLine("Pessoa está notificando: " + Mensagem);
+        }
+    }
+}
---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    // UI
+    class Program
+    {
+        //associação concreta
+        //private static EMail email = new EMail();
+        //private static SMS sms = new SMS();
+
+        //associação para abstração
+        private static INotificacao notificacao;
+
+        static void Main(string[] args)
+        {
+            string Mensagem = "Ola Mundo";
+            notificacao = new Funcionario();
+            notificacao.Enviar(Mensagem); //delegação
+            Console.ReadLine();
+        }
+    }
+}
