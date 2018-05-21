00afc94dc8322e26ff298a4ca4af621a905481b8
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-21 14:21:12 -0300

    Introdução Lambda

---
+++ Lambda/Lambda/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+   public delegate void PagarNotify(double valor);
+   public class Pedido
+   {
+        //ponteiro para um método
+        //forma de injetar código no framework
+        //código da implementação vem de fora
+        public event PagarNotify PagarEvent;
+
+        public void Pagar(double valor)
+        {
+            //delegação
+            this.PagarEvent(valor);
+        }
+   }
+
+    
+}
---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 2500;
+            var pedido = new Pedido();
+            //pedido.PagarEvent += new PagarNotify(PagarBoleto);
+            pedido.PagarEvent += v => Console.WriteLine("Pago no cartão valor de: " + v);
+            pedido.Pagar(valor);
+            Console.ReadLine();
+        }
+
+        public static void PagarBoleto(double valor)
+        {
+            Console.WriteLine("Pago no boleto valor de: " + valor);
+        }
+    }
+}
