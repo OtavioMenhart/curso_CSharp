6571b1bad67c0e2b3e8c657485f19ebe7e2f9c41
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-22 12:37:56 -0300

    Coleções Genéricas

---
+++ ColecoesGenericas/ColecoesGenericas/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ColecoesGenericas
+{
+    public class Aluno
+    {
+        public int Matricula;
+        public string Nome;
+        public string Curso;
+
+        public override string ToString()
+        {
+            return Matricula.ToString() + " - " + Nome + " - " + Curso;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alunos = new List<Aluno>();
+            alunos.Add(new Aluno() { Matricula = 1, Nome = "Otávio", Curso = "SI" });
+            alunos.Add(new Aluno() { Matricula = 2, Nome = "Menhart", Curso = "CC" });
+            alunos.Add(new Aluno() { Matricula = 3, Nome = "Ballonje", Curso = "SI" });
+            alunos.Add(new Aluno() { Matricula = 4, Nome = "Rocha", Curso = "CC" });
+            foreach (Aluno item in alunos)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Iforme o curso (CC ou SI)");
+            string curso = Console.ReadLine();
+            var qry = from aluno in alunos
+                      where aluno.Curso == curso
+                      select aluno;
+            foreach (Aluno aluno in qry)
+            {
+                Console.WriteLine(aluno);
+            }
+            Console.ReadKey();
+        }
+
+        private static void ExemploArrayList()
+        {
+            var alunos = new ArrayList();
+            alunos.Add(1);
+            alunos.Add("Otávio");
+            alunos.Add(new SqlConnection()); //aceita qualquer coisa
+            foreach (var item in alunos)
+            {
+                Console.WriteLine(item);
+            }
+            var obj = alunos[2]; // retira *sempre* um object
+            if (obj is SqlConnection)
+            {
+                (obj as SqlConnection).Open();
+            }
+        }
+    }
+}
