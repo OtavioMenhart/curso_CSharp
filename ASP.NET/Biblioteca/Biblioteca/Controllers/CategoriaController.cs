384096895d619d4f7c0a505aa61d4159a76e84ca
Otavio Menhart <otaviomenhart@hotmail.com>
2018-05-30 16:42:04 -0300

    Inserção e listagem de categorias

---
+++ ASP.NET/Biblioteca/Biblioteca/Controllers/CategoriaController.cs
@@ -0,0 +1,92 @@
+﻿using Biblioteca.DataContext;
+using Biblioteca.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private BibliotecaDB db = new BibliotecaDB();
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Categoria/Details/5
+        public ActionResult Details()
+        {
+            List<Categoria> listaCategoria = db.Categorias.ToList();
+            return View(listaCategoria);
+        }
+
+        // GET: Categoria/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categoria/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categorias.Add(categoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        // GET: Categoria/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Categoria/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Categoria/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Categoria/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- ASP.NET/Biblioteca/Biblioteca/Models/Categoria.cs
+++ ASP.NET/Biblioteca/Biblioteca/Models/Categoria.cs
@@ -1,8 +1,11 @@
-﻿namespace Biblioteca.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Models
 {
     public class Categoria
     {
         public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
     }
 }
