8871a50f3bd9eaae9b479c472ab8a488e614afc9
OtavioMenhart <otaviomenhart@hotmail.com>
2018-07-05 15:27:34 -0300

    Listagem de notas

--- ClienteREST/ClienteREST/Controllers/NotesController.cs
+++ ClienteREST/ClienteREST/Controllers/NotesController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ClienteREST.Models.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,24 @@ namespace ClienteREST.Controllers
 {
     public class NotesController : Controller
     {
+        HttpClient client = new HttpClient();
+
+        public NotesController()
+        {
+            client.BaseAddress = new Uri("http://devmedianotesapi.azurewebsites.net/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
         // GET: Notes
         public ActionResult Index()
         {
-            return View();
+            List<Notes> notes = new List<Notes>();
+            HttpResponseMessage response = client.GetAsync("api/notes").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                notes = response.Content.ReadAsAsync<List<Notes>>().Result;
+            }
+            return View(notes);
         }
 
         // GET: Notes/Details/5
