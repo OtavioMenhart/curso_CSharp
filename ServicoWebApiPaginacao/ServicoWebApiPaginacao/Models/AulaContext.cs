c609942ea8a5cda90ea7b022789aa7d21eab20e8
Otavio Menhart <otaviomenhart@hotmail.com>
2018-07-19 13:34:32 -0300

    Início do repositório

---
+++ ServicoWebApiPaginacao/ServicoWebApiPaginacao/Models/AulaContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ServicoWebApiPaginacao.Models
+{
+    public class AulaContext : DbContext
+    {
+        public AulaContext() : base("DevMediaLocal")
+        {
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+    }
+}
