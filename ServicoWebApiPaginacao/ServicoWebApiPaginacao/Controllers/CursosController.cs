2ff2241a7b709d230f0d9cc42202ca6ba2a2dae0
Otavio Menhart <otaviomenhart@hotmail.com>
2018-07-19 16:01:51 -0300

    Criação do Controller

---
+++ ServicoWebApiPaginacao/ServicoWebApiPaginacao/Controllers/CursosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ServicoWebApiPaginacao.Models;
+
+namespace ServicoWebApiPaginacao.Controllers
+{
+    public class CursosController : ApiController
+    {
+        private AulaContext db = new AulaContext();
+
+    }
+}
