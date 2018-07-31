e2175606f63480699ed55c04681c9dd055850e25
Otavio Menhart <otaviomenhart@hotmail.com>
2018-07-31 17:36:17 -0300

    Projeto utilizando recursos de layout

---
+++ Xamarin/App04_ProjetoLayout/App04_ProjetoLayout/App04_ProjetoLayout/Master/Menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App04_ProjetoLayout.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoPaginaXamarin(object sender, EventArgs args)
+        {
+            IsPresented = false;
+            Detail = new NavigationPage(new Pages.Xamarin());
+        }
+
+        private void GoPaginaPerfil1(object sender, EventArgs args)
+        {
+            IsPresented = false;
+            Detail = new NavigationPage(new Pages.Perfil1());
+        }
+
+    }
+}
