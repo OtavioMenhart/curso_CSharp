c3291d4d49d0e59720027454f9e18d9f1e4dd0d8
Otavio Menhart <otaviomenhart@hotmail.com>
2018-08-02 16:47:48 -0300

    Células e Componentes

---
+++ Xamarin/App07_Celulas/App07_Celulas/App07_Celulas/Paginas/ListViewButtonPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using App07_Celulas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App07_Celulas.Paginas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListViewButtonPage : ContentPage
+	{
+		public ListViewButtonPage ()
+		{
+			InitializeComponent ();
+
+            List<Funcionario> Lista = new List<Funcionario>();
+            Lista.Add(new Funcionario() { Nome = "Otávio", Cargo = "Desenvolvedor" });
+            Lista.Add(new Funcionario() { Nome = "Elaine", Cargo = "Gerente Vendas" });
+            Lista.Add(new Funcionario() { Nome = "Maria", Cargo = "Marketing" });
+            Lista.Add(new Funcionario() { Nome = "João", Cargo = "Vendedor" });
+
+            ListaFuncionarios.ItemsSource = Lista;
+        }
+
+        private void FeriasAction(object sender, ClickedEventArgs args)
+        {
+            Button btnFerias = (Button)sender;
+            Funcionario func = (Funcionario)btnFerias.CommandParameter;
+
+            DisplayAlert("Férias", "Funcionário: " + func.Nome, "OK");
+        }
+
+    }
+}
