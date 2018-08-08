d1f5d957081d60ec14f1a0516fc8f60dba43201e
Otavio Menhart <otaviomenhart@hotmail.com>
2018-08-08 17:15:43 -0300

    Consumindo serviço SOAP

---
+++ Xamarin/App14_SOAPClient/App14_SOAPClient/App14_SOAPClient/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App14_SOAPClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void EnviarSOAP(object sender, EventArgs args)
+        {
+            var num1 = int.Parse(Num1.Text);
+            var num2 = int.Parse(Num2.Text);
+
+            resultado.Text = DependencyService.Get<IServiceSOAP>().Somar(num1, num2);
+        }
+    }
+}
