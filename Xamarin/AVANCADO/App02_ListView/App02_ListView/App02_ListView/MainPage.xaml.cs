2d46cb667efcfeb52c01b4a2f922d9b4d5a3f058
Otavio Menhart <otaviomenhart@hotmail.com>
2018-09-04 11:55:59 -0300

    ListView Avançado (Grupos e seletor de template)

---
+++ Xamarin/AVANCADO/App02_ListView/App02_ListView/App02_ListView/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App02_ListView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ListaFuncionarios.ItemsSource = GetFuncionarios();
+
+        }
+
+        private List<Grupo> GetFuncionarios()
+        {
+            return new List<Grupo>
+            {
+                new Grupo("Presidente", "CEO", "Presidente da empresa")
+                {
+                    new Pessoa { Nome = "Otávio", IsRequired=true, RankEficiencia=8 }
+                },
+                new Grupo("Diretores", "Dir", "Diretor da empresa")
+                {
+                    new Pessoa{Nome = "João", IsRequired=false},
+                    new Pessoa{Nome = "Maria", IsRequired=true, RankEficiencia=6}
+                },
+                new Grupo("Gerentes", "Ger", "Gerente da empresa")
+                {
+                    new Pessoa{Nome = "José", IsRequired=true, RankEficiencia=9},
+                    new Pessoa{Nome = "Judas", IsRequired=false}
+                },
+                new Grupo("Funcionário", "Func", "Funcionários da empresa")
+                {
+                    new Pessoa{Nome = "José", IsRequired=false},
+                    new Pessoa{Nome = "Judas", IsRequired=false},
+                    new Pessoa{Nome = "João", IsRequired=false, RankEficiencia=6},
+                    new Pessoa{Nome = "Maria", IsRequired=false},
+                    new Pessoa{Nome = "Felipe", IsRequired=false},
+                    new Pessoa{Nome = "Danilo", IsRequired=false},
+                    new Pessoa{Nome = "Jessica", IsRequired=false},
+                    new Pessoa{Nome = "Debora", IsRequired=false}
+                },
+            };
+        }
+
+        public class Grupo : List<Pessoa>
+        {
+            public string Titulo { get; set; }
+            public string TituloCurto { get; set; }
+            public string Descricao { get; set; }
+
+            public Grupo(string titulo, string tituloCurto, string descricao)
+            {
+                this.Titulo = titulo;
+                this.TituloCurto = tituloCurto;
+                this.Descricao = descricao;
+            }
+        }
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public int RankEficiencia { get; set; }
+            public bool IsRequired { get; set; }
+        }
+    }
+}
